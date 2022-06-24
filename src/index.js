adff61ae348a30c7348059901617dc01a853a75f
Peter Vang <vang_peter06@yahoo.com>
2022-06-24 11:28:08 -0700

    WIP restarting project over since I cannot find why it won't bundle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { SolarCal } from '../src/solarCal.js';
-import $ from 'jquery';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './../css/styles.css';
+import SolarCal from './solarCal.js'
+// import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './../css/styles.css';
